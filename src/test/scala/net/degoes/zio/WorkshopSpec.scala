71b3b0c9507d3ad5e9acf96ff1ffd6a8206b473c
Zach Albia <zach.albia@gmail.com>
2019-12-27 13:23:33 +0800

    refactor: factor out resetClock, clean up property clear-befores

--- src/test/scala/net/degoes/zio/WorkshopSpec.scala
+++ src/test/scala/net/degoes/zio/WorkshopSpec.scala
@@ -8,7 +8,6 @@ import zio.duration._
 import zio.random.Random
 import zio.system.System
 import zio.test.Assertion._
-import zio.test.TestAspect._
 import zio.test._
 import zio.test.environment._
 
@@ -38,33 +37,33 @@ object WorkshopSpec
         testM("PromptName greets with name") {
           checkM(Gen.alphaNumericString) {
             name =>
-              clearConsole *>
-                (for {
-                  _                  <- TestConsole.feedLines(name)
-                  exitCode           <- PromptName.run(Nil)
-                  output             <- TestConsole.output
-                  (prompt, greeting) = (output(0), output(1))
-                } yield
-                  assert(exitCode, equalTo(0)) &&
-                    assert(prompt.toLowerCase, isNonEmptyString) &&
-                    assert(output.size, equalTo(2)) &&
-                    assert(greeting, equalTo(s"Hello, $name!\n")))
+              for {
+                _                  <- clearConsole
+                _                  <- TestConsole.feedLines(name)
+                exitCode           <- PromptName.run(Nil)
+                output             <- TestConsole.output
+                (prompt, greeting) = (output(0), output(1))
+              } yield
+                assert(exitCode, equalTo(0)) &&
+                  assert(prompt.toLowerCase, isNonEmptyString) &&
+                  assert(output.size, equalTo(2)) &&
+                  assert(greeting, equalTo(s"Hello, $name!\n"))
           }
         },
         suite("NumberGuesser")(
           testM("correct guess prints congratulatory message") {
             checkM(Gen.int(0, 100)) { num =>
-              clearConsole *> clearRandom *>
-                (for {
-                  _        <- TestRandom.feedInts(num)
-                  _        <- TestConsole.feedLines(num.toString)
-                  exitCode <- NumberGuesser.run(Nil)
-                  output   <- TestConsole.output
-                  response = output(1)
-                } yield
-                  assert(exitCode, equalTo(0)) &&
-                    assert(output.size, equalTo(2)) &&
-                    assert(response, equalTo("You guessed correctly!\n")))
+              for {
+                _        <- clearConsole *> clearRandom
+                _        <- TestRandom.feedInts(num)
+                _        <- TestConsole.feedLines(num.toString)
+                exitCode <- NumberGuesser.run(Nil)
+                output   <- TestConsole.output
+                response = output(1)
+              } yield
+                assert(exitCode, equalTo(0)) &&
+                  assert(output.size, equalTo(2)) &&
+                  assert(response, equalTo("You guessed correctly!\n"))
             }
           },
           testM("incorrect guess reveals number") {
@@ -75,7 +74,8 @@ object WorkshopSpec
             } yield (num, guess)
             checkM(genNumAndGuess) {
               case (num, guess) =>
-                clearConsole *> clearRandom *> (for {
+                for {
+                  _        <- clearConsole *> clearRandom
                   _        <- TestRandom.feedInts(num)
                   _        <- TestConsole.feedLines(guess.toString)
                   exitCode <- NumberGuesser.run(Nil)
@@ -83,7 +83,7 @@ object WorkshopSpec
                   tokens   = output(1).split(" ").map(_.strip).toList
                 } yield
                   assert(exitCode, equalTo(0)) &&
-                    assert(tokens, exists(startsWith(num.toString))))
+                    assert(tokens, contains(num.toString))
             }
           }
         ),
@@ -95,43 +95,45 @@ object WorkshopSpec
             } yield (badInputs.toVector :+ goodInput, goodInput)
             checkM(tries) {
               case (tries, goodTry) =>
-                clearConsole *> (for {
-                  _        <- TestConsole.feedLines(tries: _*)
-                  _        <- TestClock.adjust(goodTry.toInt.seconds)
-                  exitCode <- AlarmApp.run(Nil)
-                  output   <- TestConsole.output
-                } yield
-                // program always succeeds because of retry logic
-                assert(exitCode, equalTo(0)) &&
-                  // lines printed to console = prompts + wake message
-                  assert(output.size, equalTo(tries.size + 1)) &&
-                  assert(output.last.strip, equalTo("Wake up!")))
-                  .provideSomeManaged(newTestClock)
+                resetClock {
+                  for {
+                    _        <- clearConsole
+                    _        <- TestConsole.feedLines(tries: _*)
+                    _        <- TestClock.adjust(goodTry.toInt.seconds)
+                    exitCode <- AlarmApp.run(Nil)
+                    output   <- TestConsole.output
+                  } yield
+                  // program always succeeds because of retry logic
+                  assert(exitCode, equalTo(0)) &&
+                    // lines printed to console = prompts + wake message
+                    assert(output.size, equalTo(tries.size + 1)) &&
+                    assert(output.last.strip, equalTo("Wake up!"))
+                }
             }
           },
           testM("Never wakes up before alarm goes off") {
             val times = for {
               sleepTime   <- Gen.int(1, Int.MaxValue)
-              beforeAlarm <- Gen.int(1, sleepTime - 1)
+              beforeAlarm <- Gen.int(0, sleepTime - 1)
             } yield (sleepTime, beforeAlarm)
             checkM(times) {
               case (sleepTime, beforeAlarm) =>
-                clearConsole *> (for {
-                  _      <- TestConsole.feedLines(sleepTime.toString)
-                  _      <- TestClock.adjust(beforeAlarm.seconds)
-                  fiber  <- AlarmApp.run(Nil).fork
-                  _      <- waitForSleep()
-                  exit   <- fiber.interrupt
-                  output <- TestConsole.output
-                } yield
-                // assert alarm has not gone off
-                assert(exit, isInterrupted) &&
-                  // only prompt message was written to console
-                  assert(output.size, equalTo(1)))
-                  .provideSomeManaged(newTestClock)
+                resetClock {
+                  for {
+                    _      <- clearConsole
+                    _      <- TestConsole.feedLines(sleepTime.toString)
+                    _      <- TestClock.adjust(beforeAlarm.seconds)
+                    fiber  <- AlarmApp.run(Nil).fork
+                    _      <- waitForSleep()
+                    exit   <- fiber.interrupt
+                    output <- TestConsole.output
+                  } yield
+                    assert(exit, isInterrupted) && // alarm has not gone off
+                      assert(output.size, equalTo(1)) // only prompt message written
+                }
             }
           }
-        ) @@ timeout(10.seconds),
+        ),
         suite("Board")(
           test("won horizontal first") {
             horizontalFirst(Mark.X) && horizontalFirst(Mark.O)
@@ -171,24 +173,22 @@ object PropertyHelpers {
       TestRandom.clearDoubles *> TestRandom.clearFloats *>
       TestRandom.clearLongs *> TestRandom.clearStrings
 
-  def newTestClock = {
-    for {
-      env     <- ZIO.environment[zio.ZEnv with TestConsole].toManaged_
-      clock   <- TestClock.make(TestClock.DefaultData)
-      testEnv = useNewClock(env, clock)
-    } yield testEnv
-  }
-
-  def useNewClock(env: zio.ZEnv with TestConsole, testClock: TestClock) = {
-    new TestConsole with TestClock with Clock with Console with System with Random with Blocking {
-      override val console: TestConsole.Service[Any] = env.console
-      override val blocking: Blocking.Service[Any]   = env.blocking
-      override val random: Random.Service[Any]       = env.random
-      override val system: System.Service[Any]       = env.system
-      override val clock: TestClock.Service[Any]     = testClock.clock
-      override val scheduler: TestClock.Service[Any] = testClock.scheduler
+  def resetClock(test: ZIO[TestConsole with TestClock with zio.ZEnv, Nothing, TestResult]) =
+    test.provideSomeManaged {
+      for {
+        env      <- ZIO.environment[zio.ZEnv with TestConsole].toManaged_
+        newClock <- TestClock.make(TestClock.DefaultData)
+        testEnv = new TestConsole with TestClock with Clock with Console with System with Random
+        with Blocking {
+          override val console: TestConsole.Service[Any] = env.console
+          override val blocking: Blocking.Service[Any]   = env.blocking
+          override val random: Random.Service[Any]       = env.random
+          override val system: System.Service[Any]       = env.system
+          override val clock: TestClock.Service[Any]     = newClock.clock
+          override val scheduler: TestClock.Service[Any] = newClock.scheduler
+        }
+      } yield testEnv
     }
-  }
 
   def waitForSleep(): ZIO[TestClock, Nothing, Unit] =
     for {
